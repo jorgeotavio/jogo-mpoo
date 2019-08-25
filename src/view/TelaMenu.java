1212b9a3663d206ad1578b69a8692b10cd421a79
jorgeotavio <jorgelosb@gmail.com>
2019-08-25 08:51:27 -0300

    Update TelaMenu.java

--- src/view/TelaMenu.java
+++ src/view/TelaMenu.java
@@ -1,5 +1,17 @@
 package view;
 
-public class TelaMenu {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+public class TelaMenu extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JLabel titleLabel;
+	private JButton startButton, leaveButton;
+	
+	
 }
