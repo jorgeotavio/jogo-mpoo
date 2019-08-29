f3218ef5a31e92e2b48f90d202f9fe48a3a9c62d
jorgeotavio <jorgelosb@gmail.com>
2019-08-28 22:16:41 -0300

    Update ViewAbout.java

--- src/view/ViewAbout.java
+++ src/view/ViewAbout.java
@@ -9,20 +9,20 @@ public class ViewAbout extends JFrame {
 	private JButton backButton;
 	
 	public ViewAbout() {
-		setSize(400, 400);
+		setSize(400, 300);
 		setLocationRelativeTo(null);
 		setLayout(null);
 		setUndecorated(true);
 		
-		descriptionLabel = new JLabel("<html>Jogo feito pelo aluno: <br/>"
-				+ "Jorge Luiz Otávio da Silva Brito,<br/> "
-				+ "para a cadeira "
-				+ "de Mpoo, da UAST-UFRPE.<br/>"
-				+ "Professor: Rico D'Emery");
-		descriptionLabel.setBounds(90, 10, 300, 300);
+		descriptionLabel = new JLabel("<html><div style='font-size: 16; padding: 10px'>"
+				+ "Jogo feito pelo aluno, <br/>"
+				+ "Jorge Luiz Otávio da Silva Brito,<br/>"
+				+ "para a cadeira de Mpoo, da UAST-UFRPE.<br/>"
+				+ "Professor: Rico D'Emery</div>");
+		descriptionLabel.setBounds(50, -30, 300, 300);
 		
 		backButton = new JButton("Back");
-		backButton.setBounds(165, 350, 80, 30);
+		backButton.setBounds(165, 250, 80, 30);
 		
 		add(descriptionLabel);
 		add(backButton);
