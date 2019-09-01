4bb310a3574674fec16be2071e1e5f3afcaa3a43
jorgeotavio <jorgelosb@gmail.com>
2019-08-31 23:24:55 -0300

    create Hero and Enemy

---
+++ src/model/Enemy.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Enemy extends Sprite{
+
+	public Enemy(String file, int appearance, int columns, int rows, int posX, int posY) {
+		super(file, appearance, columns, rows, posX, posY);
+	}
+
+}
