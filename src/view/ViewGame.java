6c8a5fa5bde65e7392e52ed33de7ca428b7302b3
jorge-pc-casa <jorgelosb@gmail.com>
2019-09-06 12:41:56 -0300

    Update ViewGame.java

--- src/view/ViewGame.java
+++ src/view/ViewGame.java
@@ -1,8 +1,28 @@
 package view;
 
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
 
+import model.ModelEnemy;
+import model.ModelHero;
+import model.ModelMap;
+import model.ModelPlayer;
+
 public class ViewGame extends JFrame{
+	
+	private ArrayList<ModelHero> heros; 
+	private ArrayList<ModelEnemy> enemies;
+	private ArrayList<ModelPlayer> players;
+	private ArrayList<ModelMap> maps;
+	
+	public ViewGame(ArrayList<ModelHero> heros, ArrayList<ModelEnemy> enemies, ArrayList<ModelPlayer> players, ArrayList<ModelMap> maps) {
+		this.heros = heros;
+		this.enemies = enemies;
+		this.players = players;
+		this.maps = maps;
+		
+	}
 	
 	
 }
