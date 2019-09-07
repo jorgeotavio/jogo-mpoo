b910d01cf946b658af879784ee942e1a657d1f9f
jorge-pc-casa <jorgelosb@gmail.com>
2019-09-07 19:56:54 -0300

    Create ModelXml.java

---
+++ src/model/ModelXml.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.Dom4JDriver;
+
+public class ModelXml {
+
+	public ModelXml() {
+		
+	}
+
+}
