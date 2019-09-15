9f81a974718cdf6856c60d8bf93aca48273c6db7
jorge-pc-casa <jorgelosb@gmail.com>
2019-09-15 19:45:20 -0300

    Update Enemy.java

--- src/model/Enemy.java
+++ src/model/Enemy.java
@@ -1,9 +1,15 @@
 package model;
 
-public class Enemy extends Sprite{
+public class Enemy extends Sprite implements Mobile{
 
 	public Enemy(String file, int appearance, int columns, int rows, int posX, int posY) {
 		super(file, appearance, columns, rows, posX, posY);
+	}
+
+	@Override
+	public void mexer() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
