cf72063480282778e66dd9480dee057bee98ac59
Jorge Otávio <jorgelosb@gmail.com>
2019-11-21 19:41:47 -0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,16 +10,16 @@ import view.ViewMenu;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		GamePanel gp = new GamePanel();
 		ViewGame viewGame = new ViewGame(gp);
-		
+
 		RegistrarJogo registrarJogo = new RegistrarJogo(viewGame);
 		registrarJogo.registerMap();
 		registrarJogo.registerPlayer();
-		
+
 		ViewMenu viewMenu = new ViewMenu();
-		
+
 		ControllerMenu cm = new ControllerMenu(viewMenu ,registrarJogo.getViewGame());
 		
 	}
