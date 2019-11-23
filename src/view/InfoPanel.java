706720cbaf64ada5c2d5ebded7c512987635e095
Jorge Otávio <jorgelosb@gmail.com>
2019-11-23 11:25:49 -0300

    Update InfoPanel.java

--- src/view/InfoPanel.java
+++ src/view/InfoPanel.java
@@ -8,31 +8,30 @@ import javax.swing.JPanel;
 import model.Player;
 
 public class InfoPanel extends JPanel {
-	
+
 	private ArrayList<JLabel> pontuacoes;
-	
+
 	public InfoPanel() {
 		setLayout(null);
 		pontuacoes = new ArrayList<JLabel>();
-		
-		JLabel pontosPlayer1 = new JLabel();
-		pontosPlayer1.setBounds(10, 10, 100, 100);
-		
-		JLabel pontosPlayer2 = new JLabel();
-		pontosPlayer2.setBounds(10, 30, 100,100);
-		
-		pontuacoes.add(pontosPlayer1);
-		pontuacoes.add(pontosPlayer2);
-		
-		pontuacoes.forEach((pontuacao) -> {
-			pontuacao.setForeground(new Color(255,255,255));
-			add(pontuacao);
-		});
 	}
 
 	public void atualizarPontuacao(ArrayList<Player> players) {
 		players.forEach((player) -> {
 			this.pontuacoes.get(players.indexOf(player)).setText(Integer.toString(player.getPoints()));
+		});
+	}
+
+	public void cadastrarLabels(ArrayList<Player> players) {
+		for (int i = 0; i < players.size();i++) {
+			JLabel pontosLabel = new JLabel();
+			pontosLabel.setBounds(10, i*10, 100, 100);
+			pontuacoes.add(pontosLabel);
+		}
+
+		pontuacoes.forEach((pontuacao) -> {
+			pontuacao.setForeground(new Color(255,255,255));
+			add(pontuacao);
 		});
 	}
 
