559f682992d6b14182318661df0b2b01d5ab7f0d
Jorge Otávio <jorgelosb@gmail.com>
2019-11-24 17:23:05 -0300

    Update RegistrarJogo.java

--- src/model/RegistrarJogo.java
+++ src/model/RegistrarJogo.java
@@ -3,6 +3,7 @@ package model;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 import com.sun.glass.events.KeyEvent;
 
@@ -10,54 +11,62 @@ import view.GamePanel;
 import view.ViewGame;
 
 public class RegistrarJogo {
-	
+
 	private static ViewGame viewGame = new ViewGame();
-	
+
 	public static void registerMap() {
 		try {
 			Camada mapa1_camada1 = new Camada(23, 42, 16, 16, "img/mapa/tileset.png", "img/mapa/mapa_novo/camada_01.txt");
 			Camada mapa1_camada2 = new Camada(23, 42, 16, 16, "img/mapa/tileset.png",  "img/mapa/mapa_novo/camada_02.txt");
 			Camada mapa1_camada3 = new Camada(23, 42, 16, 16, "img/mapa/tileset.png", "img/mapa/mapa_novo/camada_03.txt");
-			
+
 			mapa1_camada3.setCamadaColisao(true);
-			
+
 			ArrayList<Camada> camadas = new ArrayList<Camada>();
 
 			camadas.add(mapa1_camada1);
 			camadas.add(mapa1_camada2);
 			camadas.add(mapa1_camada3);
-			
-			Objeto objeto1 = new Objeto("maca", "img/itens/maca_item.png", 200, 200);
-			Objeto objeto2 = new Objeto("banana", "img/itens/banana_item.png", 300, 200);
-			Objeto objeto3 = new Objeto("melancia", "img/itens/melancia_item.png", 350, 100);
-			Objeto objeto4 = new Objeto("maca", "img/itens/maca_item.png", 500, 140);
-			Objeto objeto5 = new Objeto("melancia", "img/itens/melancia_item.png", 130, 250);
-			
+
+			Random random = new Random();
 			ArrayList<Objeto> itens = new ArrayList<Objeto>();
-			itens.add(objeto1);
-			itens.add(objeto2);
-			itens.add(objeto3);
-			itens.add(objeto4);
-			itens.add(objeto5);
 			
+			String[] frutas = new String[5];
+			frutas[0] = "img/itens/maca_item.png";
+			frutas[1] = "img/itens/melancia_item.png";
+			frutas[2] = "img/itens/banana_item.png";
+			
+			int[][] coordenadas = new int[20][2];
+
+			for (int i = 0; i<20; i++) {
+				for (int j = 0 ;j <= 1 ; j++) {
+					if (j == 0)
+						coordenadas[i][j] = random.nextInt(450)+100;
+					else
+						coordenadas[i][j] = random.nextInt(200)+50;
+				}
+				itens.add(new Objeto("maca", frutas[random.nextInt(3)],coordenadas[i][0], coordenadas[i][1]));
+			}
+
+
 			Map mapa = new Map(camadas, itens);
 			mapa.setActivated(true);
-			
+
 			Map mapa2 = new Map(camadas, itens);
 			mapa2.setActivated(false);
 
 			ArrayList<Map> maps = new ArrayList<Map>();
 			maps.add(mapa);
 			maps.add(mapa2);
-			
+
 			RegistrarJogo.viewGame.getGamePanel().setMaps(maps);
 			RegistrarJogo.viewGame.getGamePanel().getMaps().forEach(map->map.getCamadas().forEach(camada->camada.montarMapa(680, 380)));
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void registerPlayer() {
 
 		Player player = new Player("Joao");
@@ -69,7 +78,7 @@ public class RegistrarJogo {
 		comandos1.put("RIGHT", KeyEvent.VK_RIGHT);
 		Hero hero = new Hero(sprite, comandos1);
 		player.setHero(hero);
-		
+
 		Player player2 = new Player("Maria");
 		Sprite sprite2 = new Sprite("img/sprites/heroina__.png", 2, 6, 4, 24, 200);
 		HashMap<String, Integer> comandos2 = new HashMap<String, Integer>();
@@ -79,16 +88,16 @@ public class RegistrarJogo {
 		comandos2.put("RIGHT", KeyEvent.VK_D);
 		Hero hero2 = new Hero(sprite2, comandos2);
 		player2.setHero(hero2);
-		
+
 		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(player);
 		players.add(player2);
-		
-//		Xml xml = new Xml();		
-//		xml.salvar(player);
-//		xml.salvar(player2);
-//		ArrayList<Player> players = xml.ler();
-		
+		players.add(player);
+
+		//		Xml xml = new Xml();		
+		//		xml.salvar(player);
+		//		xml.salvar(player2);
+		//		ArrayList<Player> players = xml.ler();
+
 		viewGame.getGamePanel().setPlayers(players);
 		viewGame.getInfoPanel().cadastrarLabels(players);
 	}
