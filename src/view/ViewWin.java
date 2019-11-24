7a875ec10587cc0539c0a0c68c70de9d38fc85cd
Jorge Otávio <jorgelosb@gmail.com>
2019-11-24 14:41:28 -0300

    Update ViewWin.java

--- src/view/ViewWin.java
+++ src/view/ViewWin.java
@@ -7,6 +7,7 @@ public class ViewWin extends AbstractView {
 	private JLabel mensagem;
 	
 	public ViewWin() {
+		setSize(300, 300);
 		mensagem = new JLabel("Você venceu!");
 		mensagem.setBounds(200, 200, 100, 100);
 		add(mensagem);
