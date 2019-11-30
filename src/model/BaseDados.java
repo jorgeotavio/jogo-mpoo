478a074e6f73e618fdc1eed43f12f684800f2ab8
Jorge Otávio <jorgelosb@gmail.com>
2019-11-29 22:47:24 -0300

    UPDATE

--- src/model/BaseDados.java
+++ src/model/BaseDados.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
